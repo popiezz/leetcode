4df0c1eb9ae918a2a5fb826ad5a39b3697b87236
FPops <philpopescu@proton.me>
2025-02-25 20:16:58 -0500

    added problem 26

---
+++ 0026.js
@@ -0,0 +1,17 @@
+// Remove duplicates from Sorted Array
+let array = [1, 1, 2, 2, 3, 3, 4];
+
+var removeDuplicates = function(nums) {
+  if (nums.length === 0) return 0;
+
+  let k = 1;
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] !== nums[i - 1]) {
+      nums[k] = nums[i];
+      k++;
+    }
+  }
+
+  return k;
+};
